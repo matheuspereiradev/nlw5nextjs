649ec50abd4d05f0d954e9c1b447376d5131fa18
matheus <matheuslima20111997@gmail.com>
2021-04-23 23:32:24 -0300

    paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,54 @@
-import { useEffect } from "react"
 
-export default function Home() {
+export default function Home(props) {
 
-  useEffect(()=>{
-    fetch('http://localhost:3039/episodes')
-      .then(response=> response.json() )
-      .then(data=> console.log(data))
-  },[])
+  console.log(props.episodios)
+  //modelo tradicional SPA
+  //problema não vai ser indexado
+  // useEffect(()=>{
+  //   fetch('http://localhost:3039/episodes')
+  //     .then(response=> response.json() )
+  //     .then(data=> console.log(data))
+  // },[])
+
+
 
   return (
-    <h1>index page</h1>
+    <>
+      <h1>index page</h1>
+      <p>{JSON.stringify(props.episodios)}</p>
+    </>
   )
 }
 
+//chamada via Next que diz que isso deve executar antes de carregar pagina SSR
+// SEMPRE QUE O USUÁRIO ACESSAR VAI CHAMAR NO BACKEND ===> getServerSideProps
+//É EXECUTA DE X TEMPO EM X TEMPO E SERVE UMA PAGINA ESTÁTICA =====> getStaticProps
+// export async function getServerSideProps(){
+  
+//   const response = await fetch('http://localhost:3039/episodes')
+//   const data = await response.json();
+
+
+//   return{
+//     props:{
+//       episodios: data
+//     }
+//   }
+// }
+
+export async function getStaticProps(){
+  
+  const response = await fetch('http://localhost:3039/episodes')
+  const data = await response.json();
+
+
+  return{
+    props:{
+      episodios: data
+    },
+
+    revalidate:60  //aqui define quanto tempo deve ser feita a requisição em getStaticProps
+  }
+}
 
 
