ac331b87d3ba8e4051bef300818b5cfe51fa828d
matheus <matheuslima20111997@gmail.com>
2021-04-23 23:11:29 -0300

    ouvindo server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import { Header } from '../components/header'
 import '../styles/global.scss'
+import styles from '../styles/app.module.scss'
+import { Player } from '../components/player'
 
 //sempre que muda de rota esse arquivo aqui é carregado novamente
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} /> //esse component é a pagina
+  return (
+      <div className={styles.appWrapper}>
+        <main>
+          <Header/>
+          <Component {...pageProps} /> 
+        </main>
+        <Player/>
+        
+      </div> 
+    )//esse component é a pagina
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-import { Header } from "../components/header";
+import { useEffect } from "react"
 
 export default function Home() {
+
+  useEffect(()=>{
+    fetch('http://localhost:3039/episodes')
+      .then(response=> response.json() )
+      .then(data=> console.log(data))
+  },[])
+
   return (
-    <Header/>
+    <h1>index page</h1>
   )
 }
 
